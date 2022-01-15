2d410e3a97c06ee159a202a09b26c2895425601d
astrukar <astruk00@fesb.hr>
2022-01-15 22:15:00 +0100

    add recipe detail components

---
+++ components/recipeInfo/nutritioninfo.js
@@ -0,0 +1,12 @@
+import { Row, Col } from "react-bootstrap";
+const NutritionInfo = () => {
+  return (
+    <Row className="bg-neutral-100 px-0">
+      <h1 className="flex pl-5 md:justify-center font-bold font-montserrat text-xl md:text-3xl pt-20 pb-12">
+        Nutrition Info
+      </h1>
+    </Row>
+  );
+};
+
+export default NutritionInfo;
--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -3,13 +3,19 @@ import Footer from "../../components/footer/footer";
 import { NextSeo } from "next-seo";
 import SEO from "../../data/next-seo.config";
 import { getRecipes, getSlugs } from "../../lib/datasourceAPI";
+import RecipeInfo from "../../components/recipeInfo/recipeinfo";
+import Header from "../../components/header/header";
+import headerImage from "../../public/assets/aboutBackground.png";
+import NutritionInfo from "../../components/recipeInfo/nutritioninfo";
 
 const RecipeDetailPage = ({ recipe }) => {
   return (
     <>
       <NextSeo title={`${SEO.title} - Recipe Detail`} />
       <Container fluid className={"no-gutters"}>
-        <Row>{recipe.headline}</Row>
+        <Header urlImg={headerImage} classes="md:h-44 lg:h-80" />
+        <RecipeInfo />
+        <NutritionInfo />
         <Footer />
       </Container>
     </>
