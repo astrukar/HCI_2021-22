3a7d1d7ef56b2670ea8c0ce89926bee8e0f3bfb6
emakaroglan <ekarog00@fesb.hr>
2022-01-18 23:41:37 +0100

    fix aligment on blog

--- components/blogcontainer/blogcontainer.js
+++ components/blogcontainer/blogcontainer.js
@@ -3,7 +3,7 @@ import BlogItem from "./blogitem";
 
 const BlogContainer = ({ blogPosts }) => {
   return (
-    <Row className="flex justify-center pt-24 pl-5 md:pl-10 lg:pl-16 pr-3 md:pr-10 lg:pr-16 pb-16">
+    <Row className="flex justify-center pt-24 pl-5 pr-5 md:pl-10 lg:pl-16 pr-3 md:pr-10 lg:pr-16 pb-16">
       {blogPosts.map((post) => (
         <Col xs={12} md={6} className={`mb-14 p-0 px-md-2`} key={post.sys.id}>
           <BlogItem
--- components/blogdetail/blogdetail.js
+++ components/blogdetail/blogdetail.js
@@ -3,10 +3,10 @@ import { Row, Col } from "react-bootstrap";
 const BlogDetail = ({ blogPost }) => {
   return (
     <Row>
-      <h1 className="sm:px-10 md:px-6 lg:px-16 font-bold font-montserrat text-3xl md:text-5xl py-16">
+      <h1 className="pl-5 md:px-6 lg:px-20 font-bold font-montserrat text-3xl md:text-5xl py-16">
         {blogPost.headline}
       </h1>
-      <div className="sm:px-10 md:px-6 lg:px-16 text-sm font-montserrat md:w-10/12 pb-12">
+      <div className="md:px-6 lg:px-20 text-sm font-montserrat md:w-10/12 pb-12">
         {blogPost.subheadline}
       </div>
     </Row>
